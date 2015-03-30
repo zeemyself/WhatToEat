58f236b23c31d71397ebe778e0a0edf695a7d18a
Swas Kunakorn <zeemyself@gmail.com>
2015-03-30 11:17:25 +0700

    add random

--- app/src/main/java/com/zeemyself/whattoeat/activity.java
+++ app/src/main/java/com/zeemyself/whattoeat/activity.java
@@ -1,19 +1,56 @@
 package com.zeemyself.whattoeat;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.PopupWindow;
+
+import java.util.Random;
 
 
 public class activity extends ActionBarActivity {
-
+    public String[] food = {"1","2"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity);
 
 
+    }
+    public static int randInt(int min, int max) {
+
+        // NOTE: Usually this should be a field rather than a method
+        // variable so that it is not re-seeded every call.
+        Random rand = new Random();
+
+        // nextInt is normally exclusive of the top value,
+        // so add 1 to make it inclusive
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
+        return randomNum;
+    }
+    public void wheel(){
+        ImageButton wheel = (ImageButton) findViewById(R.id.wheel);
+        wheel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String foodtext = food[randInt(0,food.length)];
+                AlertDialog.Builder pop = new AlertDialog.Builder(activity.java);
+                pop.setTitle("WE got....");
+                pop.setMessage(foodtext);
+                pop.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+            }
+        });
     }
 
 
