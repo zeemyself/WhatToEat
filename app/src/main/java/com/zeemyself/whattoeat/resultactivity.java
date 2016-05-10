caa530a24d53c1cffc988a4dd267dfab0e01ef03
Swas <zeemyself@gmail.com>
2016-05-10 11:40:44 +0700

    googlemap fragment

--- app/src/main/java/com/zeemyself/whattoeat/resultactivity.java
+++ app/src/main/java/com/zeemyself/whattoeat/resultactivity.java
@@ -1,13 +1,33 @@
 package com.zeemyself.whattoeat;
 
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 
 public class resultactivity extends AppCompatActivity {
+
+    ImageView imageResult;
+    GoogleMap map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultactivity);
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) ==
+                        PackageManager.PERMISSION_GRANTED) {
+            map.setMyLocationEnabled(true);
+//            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        }
+
     }
 }
