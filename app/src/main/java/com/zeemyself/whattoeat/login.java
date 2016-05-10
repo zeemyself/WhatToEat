34147d74336a2499f9bdd0caf49d267e703ca169
Swas <zeemyself@gmail.com>
2016-05-10 09:06:04 +0700

    Add mainmenu and result

--- app/src/main/java/com/zeemyself/whattoeat/login.java
+++ app/src/main/java/com/zeemyself/whattoeat/login.java
@@ -1,5 +1,6 @@
 package com.zeemyself.whattoeat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +19,7 @@ public class login extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(), "Yeahh", Toast.LENGTH_LONG).show();
+                startActivity(new Intent(getApplicationContext(),mainmenu.class));
             }
         });
 
--- app/src/main/java/com/zeemyself/whattoeat/resultactivity.java
+++ app/src/main/java/com/zeemyself/whattoeat/resultactivity.java
@@ -3,13 +3,11 @@ package com.zeemyself.whattoeat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class mainmenu extends AppCompatActivity {
+public class resultactivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mainmenu);
-
-
+        setContentView(R.layout.activity_resultactivity);
     }
 }
